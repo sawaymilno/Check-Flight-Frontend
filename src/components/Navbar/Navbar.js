8da299b9a759e9cbe362e85af87e16b36eb65d0f
BrettBlox <Diamond5.12>
2018-12-12 13:01:25 -0700

    syling navbar and sign up form

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Row, Navbar, NavItem } from "react-materialize";
+import { Row, Navbar, NavItem, Icon } from "react-materialize";
 
 import "./Navbar.css";
 import "../../containers/App/App.css";
 
 const Navigation = () => (
   <Row>
-   <Navbar />
+    <Navbar className="indigo darken-3" brand="Check Flight" right>
+            <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+    </Navbar>
   </Row>
 );
 
--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import { Button, Card, Row, Input, Icon } from "react-materialize";
+import { Button, Card, Row, Input, Icon, CardTitle } from "react-materialize";
 
 class Login extends Component {
   state = {};
@@ -8,24 +8,27 @@ class Login extends Component {
   render() {
     return (
       <Row>
-        <Input s={6} label="First Name" validate />
-        <Input s={6} placeholder="Last Name" />
-        <Input s={12} label="Telephone" validate type="tel">
-          <Icon>phone</Icon>
-        </Input>
-        <Input type="email" label="Email" s={12}>
-          <Icon>email</Icon>
-        </Input>
-        <Input s={12} placeholder="Username">
-          <Icon>account_circle</Icon>
-        </Input>
-        <Input placeholder="Password" type="password">
-          <Icon>lock</Icon>
-        </Input>
-        <Input placeholder="Confirm Password" type="password" />
-        <Button style={{ width: "100%" }} waves="light">
-          Submit
-        </Button>
+        <Card header="Signup">
+          
+          <Input s={6} label="First Name" validate />
+          <Input s={6} label="Last Name" validate />
+          <Input s={12} label="Telephone" validate type="tel">
+            <Icon>phone</Icon>
+          </Input>
+          <Input type="email" label="Email" s={12}>
+            <Icon>email</Icon>
+          </Input>
+          <Input s={12} label="Username">
+            <Icon>account_circle</Icon>
+          </Input>
+          <Input s={6} label="Password" type="password">
+            <Icon>lock</Icon>
+          </Input>
+          <Input s={6} label="Confirm Password" type="password" />
+          <Button style={{ width: "100%" }} waves="light">
+            Submit
+          </Button>
+        </Card>
       </Row>
     );
   }
