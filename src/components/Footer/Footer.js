036fdcfe02afa35bd79f7668c541abcf8520a792
BrettBlox <Diamond5.12>
2018-12-12 14:46:24 -0700

    changed footer color to indigo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,11 @@ import React from "react";
 
 import { Footer, Row } from "react-materialize";
 
-
 const Foot = () => (
   <Row>
     <Footer
+      className="indigo darken-3"
       copyrights="&copy; 2019"
-      className="example"
     />
   </Row>
 );
