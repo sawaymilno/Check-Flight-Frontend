b795dd122a095a362a4d81564b6ad87c8b74e84d
BrettBlox <Diamond5.12>
2018-12-15 14:25:50 -0700

    checkboxes now work and successfully update state

--- src/components/Profile/PilotSearchForm.js
+++ src/components/Profile/PilotSearchForm.js
@@ -1,44 +1,100 @@
-import React from "react";
+import { Input, Icon, Button, Row, Card } from "react-materialize";
 
-import { Input, Icon, Button, Row } from "react-materialize";
+import React, { Component } from "react";
+import PilotSearchResults from "./PilotSearchResults";
 
-const PilotSearchForm = props => {
-  const airports = props.airports;
-  const airportList = airports.map((el, i) => (
-    <Input
-      key={i}
-      s={12}
-      l={6}
-      name={el.name}
-      type="checkbox"
-      value={el.name}
-      label={el.name}
-      onChange={props.checked}
-      defaultValue={props.isChecked}
-    />
-  ));
-  return (
-    <Row style={{ textAlign: "left" }}>
-      <h6>Select Airports</h6>
-      {airportList}
-      {/* <Input s={12} label="Location: City, State">
-        <Icon>location_on</Icon>
-      </Input>
-      <Input s={12} style={{ textAlign: "left" }} label="Search Radius">
-        <Icon>location_searching</Icon>
-      </Input> */}
-      <div className="center-align">
-        <Button
-          style={{ width: "70%" }}
-          className="green accent-3"
-          type="submit"
-          onClick={props.searched}
-        >
-          Search
-        </Button>
-      </div>
-    </Row>
-  );
-};
+class PilotSearchForm extends Component {
+  state = {
+    airports: [],
+    users: []
+  };
+
+  /********************************************
+   **** Fetch Airport Data ********************
+   ********************************************/
+
+  async componentDidMount() {
+    const response = await fetch("http://localhost:3001/airports", {
+      credentials: "include"
+    });
+    const json = await response.json();
+    json.forEach((el, i) => {
+      el.isChecked = false;
+    });
+    this.setState({ ...this.state, airports: json });
+    this.getUsers();
+  }
+
+  /********************************************
+   **** Fetch User Data ********************
+   ********************************************/
+
+  getUsers = async () => {
+    const response = await fetch("http://localhost:3001/users", {
+      credentials: "include"
+    });
+    const json = await response.json();
+    this.setState({
+      ...this.state,
+      users: json
+    });
+  };
+
+  /**************************************************
+   *****  Check Boxes Click Handler *******************
+   **************************************************/
+
+  airportCheckboxHandler = e => {
+    console.log(e.target.id.slice(6));
+    const id = parseInt(e.target.id.slice(6)) - 4;
+
+    if (!this.state.airports[id].isChecked) {
+      this.setState(prevState => ({
+        ...this.state.airports.map((el, i) => {
+          if (i === id) {
+            el.isChecked = true;
+            return el;
+          }
+        })
+      }));
+    } else {
+      this.setState(prevState => ({
+        ...this.state.airports.map((el, i) => {
+          if (i === id) {
+            el.isChecked = false;
+            return el;
+          }
+        })
+      }));
+    }
+  };
+
+  render() {
+    const airports = this.state.airports;
+    const checkboxes = airports.map((airport, i) => (
+      <Input
+        s={12}
+        name={airport.name}
+        label={airport.name}
+        type="checkbox"
+        key={i}
+        onChange={this.airportCheckboxHandler}
+        checked={this.state.airports[i].isChecked}
+      />
+    ));
+    const checkedAirports = airports
+      .filter(el => el.isChecked === true)
+      .map((el, i) => <Card>{el.name}</Card>);
+    return (
+      <>
+        <Row style={{ textAlign: "left" }}>Filter Airports: {checkboxes}</Row>
+        <hr /><br/><br/>
+        <div>
+        <PilotSearchResults airports={this.state.airports} />
+        </div>
+      </>
+    );
+  }
+}
 
 export default PilotSearchForm;
