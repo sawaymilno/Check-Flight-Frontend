29ba809897e414c0dc9c26ac49ee60e8c1e59f5f
BrettBlox <Diamond5.12>
2018-12-16 13:12:50 -0700

    removed unneccessary lines of code in App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Row } from "react-materialize";
 
 /************ COMPONENT IMPORTS ******************/
 import Navigation from "../../components/Navbar/Navbar";
 import Login from "../Login/Login";
 import Foot from "../../components/Footer/Footer";
-import Calendar from "../../containers/Calendar/Calendar";
 import Intro from "../../components/Intro/Intro";
 import ExPortal from "../../containers/ExPortal/ExPortal";
 import PiPortal from "../../containers/PiPortal/PiPortal";
