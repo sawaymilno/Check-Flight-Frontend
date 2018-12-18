477cfdd2653e45496ad3d5a5f15379f644ea2a91
BrettBlox <Diamond5.12>
2018-12-18 13:23:54 -0700

    updated info section on homepage

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,24 +1,65 @@
 import React from "react";
-//import { Link } from "react-router-dom";
-import { Card, CardTitle } from "react-materialize";
+import { Card, CardTitle, Icon } from "react-materialize";
 
 import "./Intro.css";
 import "../../containers/App/App.css";
 
 const Intro = () => (
   <Card
-  header={<CardTitle image='https://flyefi.com/wp-content/uploads/2017/04/cessna-172-skyhawk-later-models-08.jpg' alt='Photo of Airplane'>Check Ride</CardTitle>}
-  // actions={[<a href='#'>This is a Link</a>]}
+    header={
+      <CardTitle
+        image="https://flyefi.com/wp-content/uploads/2017/04/cessna-172-skyhawk-later-models-08.jpg"
+        alt="Photo of Airplane"
+      >
+        Check Ride
+      </CardTitle>
+    }
+    // actions={[<a href='#'>This is a Link</a>]}
   >
-    We can always carry this a step further. There's really no end to this. And maybe a little bush lives there. We don't need any guidelines or formats. All we need to do is just let it flow right out of us. Do an almighty painting with us. Only God can make a tree - but you can paint one. It's cold, but it's beautiful.
-<br />
-<br />
-  That's the way I look when I get home late; black and blue. Mix your color marbly don't mix it dead. You can get away with a lot.
-<br />
-<br />
-  We spend so much of our life looking - but never seeing. Let's put some happy little clouds in our world. Just think about these things in your mind - then bring them into your world. You can create anything that makes you happy. You need to have a very firm paint to do this. There he comes.
-
-</Card>
+    <div className="container" style={{ textAlign: "center" }}>
+      <h4>The Easy Way To Schedule Checkrides.</h4>
+      <hr />
+      <i>
+        <h6>
+          Spend your time flying instead of scheduling. Our tool provides real
+          time examiner availability so you can quickly find the right time and
+          location for your checkride.
+        </h6>
+      </i>
+    </div>
+    <br />
+    <div
+      className="container"
+      style={{
+        paddingBottom: "1em",
+        textAlign: "center",
+        display: "grid",
+        gridGap: "20px",
+        gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))"
+      }}
+    >
+      <div>
+        <h4>Pilots</h4>
+        <hr />
+        <p>
+          Schedule your check ride with a Designated Pilot Examiner. Search
+          airports and review DPE check ride availability on your preferred
+          dates. Pick your ideal date, time, location and examiner. Once
+          selected, please allow 24 hours for DPE appointment confirmation.
+        </p>
+      </div>
+      <div>
+        <h4>Examiners</h4>
+        <hr />
+        <p>
+          Simplify the scheduling process. No more late night phone calls. Sync
+          your calendar to our online scheduling tool. Allow students to see
+          your availability and book online. Once booked, you will have 24 hours
+          to confirm the appointment and receive payment.
+        </p>
+      </div>
+    </div>
+  </Card>
 );
 
 export default Intro;
