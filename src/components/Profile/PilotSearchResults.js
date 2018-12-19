bd330b334b8c80c0df8ccfcf183727b6d9bb0d53
BrettBlox <Diamond5.12>
2018-12-18 17:20:59 -0700

    added Card component to render examiner information on Pilot profile

--- src/components/Profile/PilotSearchResults.js
+++ src/components/Profile/PilotSearchResults.js
@@ -1,41 +1,75 @@
 import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-import { Row, CollectionItem, Card, Button } from "react-materialize";
+import { Row, CollectionItem, Card, Button, Icon } from "react-materialize";
 
 class PilotSearchResults extends Component {
   state = {
     examinerViewing: null
-  }
+  };
 
-  render () {
-    console.log(this.props.airports)
+  render() {
+    console.log(this.props.airports);
 
     const filteredAirports = this.props.airports
       .filter(el => el.isChecked)
       .map((el, i) => (
-        <Card key={el.id} title={"Examiners at " + el.name} actions={
-          el.users.map((ex, i) => (
+        <Card
+          key={el.id}
+          title={"Examiners at " + el.name}
+          actions={el.users.map((ex, i) => (
             <div key={i}>
-                <CollectionItem href="#" onClick={(e) => {
-                  e.preventDefault()
-                  this.setState({ examinerViewing: ex })
-                }}>
-                  {ex.firstName + " " + ex.lastName}
-                </CollectionItem>
-                <hr />
-              </div>
-            ))
-        } />
+              <CollectionItem
+                href="#"
+                onClick={e => {
+                  e.preventDefault();
+                  this.setState({ examinerViewing: ex });
+                }}
+              >
+                {ex.firstName + " " + ex.lastName}
+              </CollectionItem>
+              <hr />
+            </div>
+          ))}
+        />
       ));
 
-    return this.state.examinerViewing ?
-    <Row>
-    {this.state.examinerViewing.firstName}
-    <Button type="button" onClick={() => {
-      this.setState({ examinerViewing: null })
-    }}>Back</Button>
-    </Row>
-    : <Row>{filteredAirports}</Row>
+    return this.state.examinerViewing ? (
+      <Row>
+        <Card>
+          <Icon large>account_circle</Icon>
+          <div>
+            <h3>
+              {this.state.examinerViewing.firstName +
+                " " +
+                this.state.examinerViewing.lastName}
+            </h3>
+            <h5>
+              <Icon>phone</Icon>&nbsp;
+              {this.state.examinerViewing.phone}
+            </h5>
+            <h5>
+              <Icon>email</Icon>&nbsp;
+              {this.state.examinerViewing.email}
+            </h5>
+            <h5>
+              <Icon>monetization_on</Icon>&nbsp;
+              {this.state.examinerViewing.rates}
+            </h5>
+          </div>
+          <Button
+            style={{ width: "50%" }}
+            className="green accent-3"
+            type="button"
+            onClick={() => {
+              this.setState({ examinerViewing: null });
+            }}
+          >
+            Back
+          </Button>
+        </Card>
+      </Row>
+    ) : (
+      <Row>{filteredAirports}</Row>
+    );
   }
 }
 
